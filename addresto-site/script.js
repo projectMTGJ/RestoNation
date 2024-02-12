2a719c339ba923b2561625e43dd38f8c1c23548a
projectMTGJ <153870499+projectMTGJ@users.noreply.github.com>
2024-02-13 00:10:33 +0100

    Add files via upload

--- addresto-site/script.js
+++ addresto-site/script.js
@@ -4,3 +4,10 @@ const fields = [
         inputType: "text"
     }
 ]
+
+const action = {
+    date: function(id) {
+        const date = new Date();
+        document.getElementById(id).value = date.getFullYear() + '-' + ((date.getMonth() + 1) < 10 ? '0' : '') + (date.getMonth() + 1) + '-' + (date.getDate() < 10 ? '0' : '') + date.getDate();
+    }
+}
